3cd6300960f537d29d29d0afba59673b3581fc9c
MrSergey <mrsergey@mail.ru>
2024-02-07 00:33:12 +0300

    qwe

---
+++ LR1.py
@@ -0,0 +1,392 @@
+import math
+import random
+
+
+def prost(a):
+    k = 1
+    for i in range(2, a):
+        if a % i == 0:
+            k += 1
+    return k == 1
+
+
+def maxprostdel(a):
+    delit = 1
+    for i in range(2, a + 1):
+        if a % i == 0 and prost(i):
+            delit = i
+    return delit
+
+
+# f2
+
+def proizv5(a):
+    p = 1
+    astr = str(a)
+    for i in astr:
+        if (int(i) % 5 != 0):
+            p *= int(i)
+    return p
+
+
+# f3
+
+def maxnechnepr(a):
+    delit = 1
+    for i in range(2, a + 1):
+        if a % 2 != 0 and a % i == 0 and not prost(i):
+            delit = i
+    return delit
+
+
+def proizv(a):
+    p = 1
+    astr = str(a)
+    for i in astr:
+        p *= int(i)
+    return p
+
+
+def nod(a, b):
+    delit = 1
+    for i in range(2, min(a, b) + 1):
+        if a % i == 0 and b % i == 0:
+            delit = i
+    return delit
+
+
+# 2-4
+
+# #3
+
+def randomstring(a):
+    q = a.split()
+    b = random.sample(range(len(q)), len(q))
+    newstr = ""
+    for i in b:
+        newstr += q[i] + " "
+    print(newstr)
+
+
+# #8
+
+
+def kolsv(a):
+    q = a.split()
+    k = 0
+    for i in q:
+        if len(a) % 2 == 0:
+            k += 1
+    return k
+
+
+# #16
+
+def flag():
+    a = ["красный", "синий", "белый"]
+    random.shuffle(a)
+    for i in range(len(a) - 1):
+        for j in range(len(a)):
+            if (i == 0 and a[j] == "белый") or (i == 1 and a[j] == "синий"):
+                k = a[i]
+                a[i] = a[j]
+                a[j] = k
+    return a
+
+
+# 5
+
+def dats(a):
+    tempa = a.split()
+    months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля",
+              "августа", "сентября", "октября", "ноября", "декабря"]
+    d = []
+    for i in range(len(tempa) - 2):
+        if tempa[i].isdigit() and tempa[i + 2].isdigit():
+            if (1 <= int(tempa[i]) and 31 >= int(tempa[i]) and tempa[i + 1] in months and 1 <= int(
+                    tempa[i + 2]) and 2025 >= int(tempa[i + 2])):
+                d.append(tempa[i] + " " + tempa[i + 1] + " " + tempa[i + 2])
+    return d
+
+
+# 6-8
+
+# #3
+
+def lenrus(a):
+    k = 0
+    for i in a:
+        if "а" <= i.lower() and "я" >= i.lower():
+            k += 1
+    return k
+
+
+# #8
+
+def leneng(a):
+    k = 0
+    for i in a:
+        if "a" <= i and "z" >= i:
+            k += 1
+    return k
+
+
+# #16
+
+def mindigit(a):
+    min = None
+    for i in range(len(a)):
+        if a[i].isdigit():
+            min = int(a[i])
+            k = i + 1
+            if k == len(a):
+                break
+            while a[k].isdigit():
+                min = int(str(min) + a[k])
+                k += 1
+                if k == len(a):
+                    break
+            break
+
+    i = 0
+
+    while i < len(a):
+        ch = ""
+        if a[i].isdigit():
+            ch = a[i]
+            k = i + 1
+            if k >= len(a):
+                if int(a[i]) < min:
+                    min = int(a[i])
+                break
+            while a[k].isdigit():
+                ch += a[k]
+                k += 1
+                if k >= len(a):
+                    break
+            if int(ch) < min:
+                min = int(ch)
+            i = k + 1
+        else:
+            i += 1
+
+    return min
+
+
+# 9
+
+def sortstringsbychars():
+    a = input("Введие строку, 0 для завершения\n")
+    b = []
+    while a != "0":
+        b.append(a)
+        a = input("Введие строку, 0 для завершения\n")
+    for i in range(len(b)):
+        for j in range(len(b) - 1):
+            if len(b[j + 1]) > len(b[j]):
+                k = b[j]
+                b[j] = b[j + 1]
+                b[j + 1] = k
+
+    return b
+
+
+# 10
+
+def sortstringsbywords():
+    a = input("Введие строку, 0 для завершения\n")
+    b = []
+    while a != "0":
+        b.append(a)
+        a = input("Введие строку, 0 для завершения\n")
+    for i in range(len(b)):
+        for j in range(len(b) - 1):
+            if len(b[j + 1].split()) > len(b[j].split()):
+                k = b[j]
+                b[j] = b[j + 1]
+                b[j + 1] = k
+    return b
+
+
+# 11-14
+
+def sortstringsbyq():
+    a = input("Введие строку, 0 для завершения\n")
+    b = []
+    while a != "0":
+        b.append(a)
+        a = input("Введие строку, 0 для завершения\n")
+    m = []
+    for string in b:
+        sl = {}
+        for char in string:
+            if char not in sl:
+                sl[char] = 1
+            else:
+                sl[char] += 1
+        m.append(sl)
+    n = {}
+    l = 0
+    for seg in m:
+        k = 0
+        for i in seg.keys():
+            if seg[i] > k:
+                k = seg[i]
+        n[l] = k
+        l += 1
+    mk = list(n.keys())
+    mv = list(n.values())
+    for i in range(len(mv)):
+        for j in range(len(mv) - 1):
+            if mv[j + 1] < mv[j]:
+                k = mv[j]
+                mv[j] = mv[j + 1]
+                mv[j + 1] = k
+                k = mk[j]
+                mk[j] = mk[j + 1]
+                mk[j + 1] = k
+    rm = []
+    for i in mk:
+        rm.append(b[i])
+    return rm
+
+# #5
+
+# #9
+
+def sortstringsbyascii():
+    a = input("Введие строку, 0 для завершения\n")
+    b = []
+    while a != "0":
+        b.append(a)
+        a = input("Введие строку, 0 для завершения\n")
+    n = {}
+    l = 0
+    for string in b:
+        k = 0
+        for char in string:
+            if ord(char) > k:
+                k = ord(char)
+        lstr = int(len(string)/2)-1
+        dif = 0
+        if len(string) % 2 == 0:
+            dif = abs(ord(string[lstr])-ord(string[lstr+1]))
+        else:
+            dif = abs(ord(string[lstr]) - ord(string[lstr + 2]))
+        o = math.sqrt(((k-dif)**2)/2)
+        n[l] = o
+        l += 1
+    mk = list(n.keys())
+    mv = list(n.values())
+    for i in range(len(mv)):
+        for j in range(len(mv) - 1):
+            if mv[j + 1] < mv[j]:
+                k = mv[j]
+                mv[j] = mv[j + 1]
+                mv[j + 1] = k
+                k = mk[j]
+                mk[j] = mk[j + 1]
+                mk[j + 1] = k
+    rm = []
+    for i in mk:
+        rm.append(b[i])
+    return rm
+
+# #11
+
+def sortstringsbyascii2():
+    a = input("Введие строку, 0 для завершения\n")
+    b = []
+    while a != "0":
+        b.append(a)
+        a = input("Введие строку, 0 для завершения\n")
+    n = {}
+    max1 = 0
+    for i in range(0, len(b[0]), 3):
+        c1 = ord(b[0][i])
+        c2 = ord(b[0][i])
+        c3 = ord(b[0][i])
+        if (c1 + c2 + c3) / 3 > max1:
+            max1 = (c1 + c2 + c3) / 3
+    l = 0
+    for string in b:
+        max = 0
+        for i in range(0, len(string)-3, 3):
+            c1 = ord(string[i])
+            c2 = ord(string[i+1])
+            c3 = ord(string[i+2])
+            if (c1+c2+c3)/3 > max:
+                max = (c1+c2+c3)/3
+        n[l] = ((max-max1)/2)**2
+        l += 1
+    mk = list(n.keys())
+    mv = list(n.values())
+    for i in range(len(mv)):
+        for j in range(len(mv) - 1):
+            if mv[j + 1] < mv[j]:
+                k = mv[j]
+                mv[j] = mv[j + 1]
+                mv[j + 1] = k
+                k = mk[j]
+                mk[j] = mk[j + 1]
+                mk[j + 1] = k
+    rm = []
+    for i in mk:
+        rm.append(b[i])
+    return rm
+
+# 15-19
+
+# #9
+def elementsprevmin(a):
+    min = a[0]
+    for i in a:
+        if i < min:
+            min = i
+    l = 0
+    for i in range(len(a)):
+        if a[i] == min:
+            l = i
+    return a[:l]
+
+# #21
+
+
+def elementsaftermax(a):
+    max1 = max(a)
+    i = a.index(max1)
+    return a[i+1:]
+
+# #33
+
+
+def proovealternation(a):
+    prov = 1
+    for i in range(len(a)-1):
+        if a[i] > 0 and a[i+1] > 0 or a[i] < 0 and a[i+1] < 0:
+            prov = 0
+    if prov == 1:
+        return True
+    return False
+
+# #45
+
+
+def sumelemininterval(m,a,b):
+    sum = 0
+    for i in m:
+        if a < i < b:
+            sum += i
+    return sum
+
+# #57
+
+
+def countelementsmorethenprev(a):
+    k = 0
+    s = 0
+    for i in a:
+        if i > s:
+            k += 1
+        s += i
+    return k
