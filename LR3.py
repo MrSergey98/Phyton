c06eaff08783e64f722eb2267736e3a84dc32103
MrSergey <mrsergey@mail.ru>
2024-02-25 20:38:35 +0300

    Функция нахождения площади у пятиугольника

--- LR3.py
+++ LR3.py
@@ -6,6 +6,9 @@ class Point:
         self.x = x
         self.y = y
 
+    def distanceTo(self, p2):
+        return math.sqrt((p2.x-self.x)**2+(p2.y-self.y)**2)
+
 
 class Triangle:
     def __init__(self, p1, p2, p3):
@@ -51,13 +54,9 @@ class Pentagon:
         self.p5.y += y
 
     def square(self):
-        a = math.sqrt((self.p1.x - self.p2.x) ** 2 + (self.p1.y - self.p2.y) ** 2)
-        b = math.sqrt((self.p2.x - self.p3.x) ** 2 + (self.p2.y - self.p3.y) ** 2)
-        c = math.sqrt((self.p3.x - self.p4.x) ** 2 + (self.p3.y - self.p4.y) ** 2)
-        d = math.sqrt((self.p4.x - self.p5.x) ** 2 + (self.p4.y - self.p5.y) ** 2)
-        e = math.sqrt((self.p5.x - self.p1.x) ** 2 + (self.p5.y - self.p1.y) ** 2)
-        p = (a+b+c+d+e)/2
-        return math.sqrt(p * (p - a) * (p - b) * (p - c) * (p - d) * (p - e))
+        sq = Geron(self.p1.distanceTo(self.p2), self.p2.distanceTo(self.p3), self.p1.distanceTo(self.p3))+Geron(self.p1.distanceTo(self.p3), self.p3.distanceTo(self.p4), self.p1.distanceTo(self.p4))+Geron(self.p1.distanceTo(self.p4),self.p4.distanceTo(self.p5), self.p1.distanceTo(self.p5))
+        print("Pentagon square: ", sq)
+        return sq
 
 
 def compare(t1, t2):
@@ -67,22 +66,30 @@ def compare(t1, t2):
         return type(t2)
 
 
+def Geron(a, b, c):
+    p=(a+b+c)/2
+    sq = math.sqrt(p*(p-a)*(p-b)*(p-c))
+    print(sq)
+    return sq
+
+
 if __name__ == '__main__':
+
     print("Inp points for Triangle")
     pt = []
     for i in range(3):
-        x = int(input("Inp x\n"))
-        y = int(input("Inp y\n"))
+        x = float(input("Inp x\n"))
+        y = float(input("Inp y\n"))
         pt.append(Point(x, y))
     tringle = Triangle(pt[0], pt[1], pt[2])
     print("Inp points for Pentagon")
     pp = []
     for i in range(5):
-        x = int(input("Inp x\n"))
-        y = int(input("Inp y\n"))
+        x = float(input("Inp x\n"))
+        y = float(input("Inp y\n"))
         pp.append(Point(x, y))
     pentagon = Pentagon(pp[0], pp[1], pp[2], pp[3], pp[4])
-    tringle.move(input("Inp x\n"), input("Inp y\n"))
-    pentagon.move(input("Inp x\n"), input("Inp y\n"))
+    #tringle.move(input("Inp x\n"), input("Inp y\n"))
+    #pentagon.move(input("Inp x\n"), input("Inp y\n"))
     print(compare(tringle, pentagon))
 
