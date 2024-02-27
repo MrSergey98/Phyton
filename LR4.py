27701d99cccf7d62df36317005ea1c01d9a545e8
MrSergey <mrsergey@mail.ru>
2024-02-27 14:07:57 +0300

    Задание 2 на нахождение слова с наибольшим количеством введённых букв.

--- LR4.py
+++ LR4.py
@@ -1,5 +1,8 @@
 import math
 
+
+# 1
+
 def commuting(path_file):
     file = open(path_file)
     tmpString = file.readline().split()
@@ -24,8 +27,37 @@ def commuting(path_file):
         right -= punkts_kolbs[i][1]
     print(min(costs))
 
+
+# 2
+
+def find_word(path_file):
+    file = open(path_file, encoding="UTF-16")
+    strings = []
+    for string in file:
+        strings.append(string[:-1])
+    literalls = []
+    literall = input("Введите букву, 0 - признак конца\n")
+    literalls.append(literall)
+    while literall != '0':
+        literall = input("Введите букву, 0 - признак конца\n")
+        literalls.append(literall)
+    ret_word = 'й'
+    sum_literalls = 0
+    for string in strings:
+        for word in string.split():
+            tmp_sum_literalls = 0
+            for lit in literalls:
+                tmp_sum_literalls += word.count(lit)
+            if tmp_sum_literalls > sum_literalls:
+                ret_word = word
+                sum_literalls = tmp_sum_literalls
+    return ret_word
+
+
 if __name__ == "__main__":
-    a = "C:\\Users\\s0169591\\Desktop\\27-122a.txt"
-    b = "C:\\Users\\s0169591\\Desktop\\27-122b.txt"
-    commuting(a)
-    commuting(b)
+    # a_file = "C:\\Users\\s0169591\\Desktop\\27-122a.txt"
+    # b_file = "C:\\Users\\s0169591\\Desktop\\27-122b.txt"
+    # commuting(a_file)
+    # commuting(b_file)
+    text_file = "text.txt"
+    print(find_word(text_file))
