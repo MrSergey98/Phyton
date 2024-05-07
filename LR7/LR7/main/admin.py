1e27113d7fdf20bec12ae2c14b22163489edf280
MrSergey <mrsergey98@mail.ru>
2024-05-07 15:13:50 +0300

    Отображение полей в панели админа

--- LR7/LR7/main/admin.py
+++ LR7/LR7/main/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Lawsuits, Responsible_for_lawsuits, Lawsuits_dates
-admin.site.register(Lawsuits)
-admin.site.register(Responsible_for_lawsuits)
-admin.site.register(Lawsuits_dates)
+
+class LawsuitsAdmin(admin.ModelAdmin):
+    list_display = ("id", "responsible", "lawsuit")
+
+class Responsible_for_lawsuitsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "info")
+
+class Lawsuits_datesAdmin(admin.ModelAdmin):
+    list_display = ("id", "date", "info")
+
+admin.site.register(Lawsuits, LawsuitsAdmin)
+admin.site.register(Responsible_for_lawsuits, Responsible_for_lawsuitsAdmin)
+admin.site.register(Lawsuits_dates, Lawsuits_datesAdmin)
 
 # Register your models here.
